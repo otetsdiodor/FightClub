774e29d77b62afcaa0487eaba93e6ce26b3cea00
Danial Shlenov <otetsdiodor@gmail.com>
2017-10-21 14:54:06 +0300

    Накидали инфы в Player

--- FightClub/Player.cs
+++ FightClub/Player.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace FightClub
 {
+    public enum BodyParts
+    {
+        Head,
+        Body,
+        Legs
+    }
     class Player
     {
+        private string Name;
+        private BodyParts Blocked;
+        private double HealthPoint;
+        public Player()
+        {
+
+        }
+
+        public void GetHit(BodyParts bodyPart)
+        {
+
+        }
+        public void SetBlock(BodyParts bodyPart)
+        {
+            Blocked = bodyPart;
+        }
+
     }
 }
