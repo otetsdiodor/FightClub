64fc5cb5bd02a515622c9bdc9f5a490a358adc7a
Danial Shlenov <otetsdiodor@gmail.com>
2017-10-21 15:33:20 +0300

    Добавлены события Wound Death Block Написан метод GetHit

--- FightClub/Player.cs
+++ FightClub/Player.cs
@@ -12,19 +12,45 @@ namespace FightClub
         Body,
         Legs
     }
+    
     class Player
     {
-        private string Name;
-        private BodyParts Blocked;
-        private double HealthPoint;
-        public Player()
-        {
+        private string Name; // Имя игрока
+        private BodyParts Blocked; // Часть тела которую мы блокируем
+        private double HealthPoint; // Текущее кол-во ХП
+        public const double ImpactForce = 25; // Дамаг которые наности какждая атака
 
+        //ПРОВЕРЬ А МОЖНО ЛИ РАБОТАТЬ С ОДНИМ ДЕЛЕГАТОМ ДЛЯ ВСЕ СОБЫТИЙ
+        public delegate void BlockDelegate(object sender, PlayerEventArgs e);
+        public event BlockDelegate Block; // Атака заблокирована
+
+        public delegate void WoundDel(object sender, PlayerEventArgs e);
+        public event WoundDel Wound; // Атака пропущена , урон получен
+
+        public delegate void DeathDel(object sender, PlayerEventArgs e);
+        public event DeathDel Death; // Вы умерли =(
+
+        public Player(string Name)
+        {
+            this.Name = Name;
         }
 
         public void GetHit(BodyParts bodyPart)
         {
-
+            if (Blocked != bodyPart)
+            {
+                HealthPoint -= ImpactForce;
+                Wound(this,new PlayerEventArgs(Name,HealthPoint));
+                
+                if (HealthPoint <=0) //Проверить : а не мертвы ли мы?
+                {
+                    Death(this,new PlayerEventArgs(Name,HealthPoint));
+                }
+            }
+            else
+            {
+                Block(this, new PlayerEventArgs(Name,HealthPoint));
+            }
         }
         public void SetBlock(BodyParts bodyPart)
         {
---
+++ FightClub/PlayerEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FightClub
+{
+    class PlayerEventArgs
+    {
+        public string Name;
+        public double HealthPoint;
+
+        public PlayerEventArgs(string Name, double HealthPoint)
+        {
+            this.Name = Name;
+            this.HealthPoint = HealthPoint;
+        }
+    }
+}
